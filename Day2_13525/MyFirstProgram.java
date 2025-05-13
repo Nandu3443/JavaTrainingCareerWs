26daa9ef9dde1940092de13a316526c891aff14e
= <=>
2025-05-13 22:13:42 +0530

    Day2_13525

---
+++ Day2_13525/MyFirstProgram.java
@@ -0,0 +1,19 @@
+
+public class MyFirstProgram {
+
+	public static void main(String[] args) {
+			System.out.println("My First Java Program...");
+			System.out.println("Hi Hello");
+			int a = 10;
+			char c = 'a';
+			boolean yesNo = true;
+			double salary = 20.000;
+			// Size of data type
+			System.out.println(Byte.SIZE);
+			System.out.println(Integer.SIZE);
+			
+			System.out.println(Byte.MIN_VALUE);
+			System.out.println(Byte.MAX_VALUE);
+	}
+
+}
