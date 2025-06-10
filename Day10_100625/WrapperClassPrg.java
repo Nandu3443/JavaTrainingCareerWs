18165ebfd62d5260f66a6e3e84ccc33b9e1d7f24
= <=>
2025-06-10 21:31:45 +0530

    Day 10 Wrapper classes

---
+++ Day10_100625/WrapperClassPrg.java
@@ -0,0 +1,12 @@
+
+public class WrapperClassPrg {
+
+	public static void main(String[] args) {
+		int a = 10;
+		Integer b = new Integer(10);
+		Long l = new Long(50000000l);
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
--- Day9_28525/Alpha.java
+++ Day9_28525/Alpha.java
@@ -3,7 +3,16 @@ class Developer{
 	int empId;
 	double salary;
 }
+
+class CalculationValues{
+	static int a =0;
+	static int b =0;
+}
 public class Alpha {
+	public static void addition() {
+		int result = CalculationValues.a+CalculationValues.b;
+	}
+		
 	public static void main(String[] args) {
 		Developer ali = new Developer();
 		ali.name = "Ibrahim Ali";
@@ -11,5 +20,8 @@ public class Alpha {
 		ali.salary = 100000;
 		int a = 10;
 		System.out.println(ali);
+		CalculationValues.a = 10;
+		CalculationValues.b = 20;
+		addition();
 	}
 }
