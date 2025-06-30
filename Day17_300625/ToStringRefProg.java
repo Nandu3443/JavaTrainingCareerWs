bd63953f634d698d21f89f9c361e3b505f64cabe
= <=>
2025-06-30 21:46:31 +0530

    Day17_300625

---
+++ Day17_300625/ToStringRefProg.java
@@ -0,0 +1,19 @@
+class AlphaClass {
+	
+	public String toString() {
+		return "This is AlphaClass";
+	}
+	
+	
+}
+public class ToStringRefProg {
+
+	public static void main(String[] args) {
+		  AlphaClass alpha = new AlphaClass();
+		  System.out.println(alpha);
+		  String str = new String("ABC");
+		  System.out.println(str);
+
+	}
+
+}
